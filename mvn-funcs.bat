bfcd026b6d0eda57291206244c470e367ff16818
jiqj <316719744@qq.com>
2021-06-11 17:15:38 +0800

    打包工具

---
+++ mvn-funcs.bat
@@ -0,0 +1,20 @@
+@echo off
+set setting=E:\works\toone-maven-settings.xml
+echo ==================maven 仓库设置(%setting%)====================
+rd /s /Q target
+mkdir target 
+
+echo =================批量打包=================
+for %%i in (
+    Serverfunc_ABS
+    Serverfunc_GetEntityRowCountFunc
+	Serverfunc_ConcatStr
+) do (
+	echo "处理:===%%i ===" 
+	call mvn clean package -f %%i\pom.xml --settings %setting%
+    call copy %%i\target\*.jar target\
+)
+   
+del target\*-javadoc.jar
+del target\*-sources.jar
+pause
