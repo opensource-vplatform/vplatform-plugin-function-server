f4b7ce861087ff77222511576b1c6f225cd9d24e
jiqj <316719744@qq.com>
2021-07-02 11:13:09 +0800

    删除未发布的构件

--- Serverfunc_CommonUtils/src/main/java/com/toone/v3/platform/function/common/ServerFuncCommonUtilsImpl.java
+++
@@ -1,8 +0,0 @@
-package com.toone.v3.platform.function.common;
-
-/**
- * @Author xugang
- * @Date 2021/6/8 21:40
- */
-public class ServerFuncCommonUtilsImpl implements ServerFuncCommonUtils {
-}
--- Serverfunc_EncryptionFunc/src/main/java/com/toone/v3/platform/function/encrypt/EncryptFactory.java
+++
@@ -1,8 +0,0 @@
-package com.toone.v3.platform.function.encrypt;
-
-/**
- * @Author xugang
- * @Date 2021/6/1 14:18
- */
-public class EncryptFactory {
-}
